69f17a71641649ed0b0aeb4c3a2d7adec588912c
Vibuddha94 <bvibuddha@gmail.com>
2025-05-13 15:51:17 +0530

    start function on login page close #3

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 import { validateName } from './validation/validation.js';
 
+
 //Validate Name Input
-window.handleKeyUp = (value) => validateName(value,"warning");
+window.handleKeyUp = (value) => {
+    if(!validateName(value)){
+        document.getElementById("warning").innerText = "Input valid name (Ex: John)"
+    } else {
+        document.getElementById("warning").innerText = "";
+    }
+};
+
+window.start = () => {
+    let name = document.getElementById('name').value;
+    if(!validateName(name)){
+        
+    } else {
+         window.location.href = `main.html?name=${encodeURIComponent(name)}`
+    }
+}
--- validation/validation.js
+++ validation/validation.js
@@ -1,9 +1,5 @@
-export function validateName(nameInput,out) { 
+export function validateName(nameInput) { 
     let name = nameInput;
     const nameRegex = /^([A-Za-z]){4,20}$/gm;
-    if(!nameRegex.test(name)){
-        document.getElementById(out).innerText = "Input valid name (Ex: John)"
-    } else {
-        document.getElementById(out).innerText = "";
-    }
+    return nameRegex.test(name);
 } 
